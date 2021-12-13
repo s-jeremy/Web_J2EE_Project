03d7e9f36c91244c5d6a489f1cb971d00716a1d9
Rechid Mehdi <64952624+mmrechid@users.noreply.github.com>
2021-12-13 11:19:30 +0100

    Notification inscription back

--- src/main/java/com/example/web_j2ee_project/servlets/Inscription.java
+++ src/main/java/com/example/web_j2ee_project/servlets/Inscription.java
@@ -10,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -46,7 +47,11 @@ public class Inscription extends HttpServlet
 
                     transaction.commit();
                     hibernateSession.close();
-                    out.println("Utilisateur enregistrer avec l'id : "+savedUserId);
+
+                    HttpSession httpSession = request.getSession();
+                    httpSession.setAttribute("notification","Inscription Réussite !");
+                    response.sendRedirect("register.jsp");
+                    return;
                 }
                 else
                 {
