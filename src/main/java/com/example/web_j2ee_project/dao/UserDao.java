d482c9d3e8ff1c5466cfdd8e3d33df689abd3992
Rechid Mehdi <64952624+mmrechid@users.noreply.github.com>
2021-12-31 17:16:59 +0100

    Ajout check si utilisateur existe déja

--- src/main/java/com/example/web_j2ee_project/dao/UserDao.java
+++ src/main/java/com/example/web_j2ee_project/dao/UserDao.java
@@ -13,16 +13,35 @@ public class UserDao {
         this.factory = factory;
     }
 
-    public Client getUserByEmailPw(String id, String pw){
+    public boolean checkIfUserExist(String username){
+        boolean exist = true;
+        try{
+
+            String query = "from Client where username =: u";
+            Session session = this.factory.openSession();
+            Query q = session.createQuery(query);
+            q.setParameter("u", username);
+            Client utilisateur=(Client) q.uniqueResult();
+
+            if (utilisateur==null){
+                exist = false;
+            }
+            session.close();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return exist;
+    }
+
+    public Client getUserByEmailPw(String username, String pw){
         Client utilisateur = null;
-
-
         try{
 
             String query = "from Client where username =: u and password =: p";
             Session session = this.factory.openSession();
             Query q = session.createQuery(query);
-            q.setParameter("u", id);
+            q.setParameter("u", username);
             q.setParameter("p",pw);
             utilisateur=(Client) q.uniqueResult();
 
