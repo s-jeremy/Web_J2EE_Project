28bae7a5a163638a6c793c65a0d25efcb0ea68b4
s-jeremy <jeremy.selo@gmail.com>
2022-01-05 16:59:31 +0100

    Gestion du panier avec script.js (Bug à résoudre...)

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -40,3 +40,59 @@ function add_product(id_produit, nom_produit, prix_produit){
         }
     }
 }
+
+
+function updatePanier(){
+
+    let panierToString = localStorage.getItem("panier");
+    let panier = JSON.parse(panierToString);
+    if(panier == null || panier.length == null){
+        console.log("Le panier est vide !");
+        $(".cart-items").html(" (0) ");
+        $(".cart-body").html("<h3>Aucun produit dans le panier !</h3>");
+        $(".checkout-btn").addClass('disabled');
+    }
+    else {
+        console.log(panier);
+        $(".cart-items").html(`( ${panier.length} )`);
+        let table = `
+        <table class='table'>
+        <thread class='thread-light'>
+            <tr>
+                <th>Produit</th>
+                <th>Prix</th>
+                <th>Quantite</th>
+                <th>Prix total</th>
+                <th>Action</th>
+            </tr>
+        </thread>
+        `;
+
+        let prixTotal = 0;
+        panier.map((item) => {
+            table += `
+                <tr>
+                    <td>{item.nom_produit}</td>
+                    <td>{item.prix_produit}</td>
+                    <td>{item.quantite_produit}</td>
+                    <td>{item.quantite_produit*item.prix_produit}</td>
+                    <td><button class='btn btn-danger btn-sm'>Supprimer</button></td>
+                </tr>
+            `
+            prixTotal += item.quantite_produit*item.prix_produit;
+        })
+
+        table = table + `
+            <tr>
+                <td colspan="5" class="text-right font-weight-bold m-5">
+                    Prix total : ${prixTotal} 
+                </td>
+            </tr>
+        </table>`
+        $(".cart-body").html(table);
+    }
+}
+
+$(document).ready(function () {
+    updatePanier();
+})
