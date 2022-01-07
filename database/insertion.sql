36baa349ae72707af5a701942782e67ece2d3aa2
Rechid Mehdi <64952624+mmrechid@users.noreply.github.com>
2022-01-07 20:10:37 +0100

    Ajout script insertion.sql MySQL

---
+++ database/insertion.sql
@@ -0,0 +1,31 @@
+INSERT INTO `j2e`.`categorie` (`titre`, `description`) VALUES ('Apple', 'Smartphone de Apple');
+INSERT INTO `j2e`.`categorie` (`titre`, `description`) VALUES ('Samsung', 'Téléphones Samsung');
+INSERT INTO `j2e`.`categorie` (`titre`, `description`) VALUES ('Xiaomi', 'Plusieurs téléphones de Xiaomi');
+INSERT INTO `j2e`.`categorie` (`titre`, `description`) VALUES ('Huawei', 'Téléphone pas trop chère');
+INSERT INTO `j2e`.`categorie` (`titre`, `description`) VALUES ('Honor', 'Le dernier type !');
+
+
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('iPhone 13', 'Le iPhone dernier cri', '999', '632', '1');
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('iPhone 12', 'Comme le iPhone 12 mais moins chère', '650', '357', '1');
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('iPhone 11', 'Mieux que le iPhone 10 et pire que le 12', '480', '956', '1');
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('iPhone 10', 'Toujours aussi performant', '360', '4', '1');
+
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('Galaxy Z Flip', 'Téléphone qui se flip', '669', '32', '2');
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('Galaxy S21 FE', 'Le meilleur du Galaxy S', '449', '211', '2');
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('Galaxy S10', 'L\'incontournable Galaxy S10', '299', '17', '2');
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('Galaxy S20+', 'Celui juste avant le S21', '319', '963', '2');
+
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('Xiaomi Mi 10T', 'Le premier model de France', '403', '33', '3');
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('Xiaomi Redmi 10S', 'Pas très chère et très performant !', '199', '205', '3');
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('Xiaomi Rednote 10S', 'Comme le Redmi mais plus chère', '399', '58', '3');
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('Xiaomi Mi 11T', 'Le meilleur du Xiaomi', '525', '2', '3');
+
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('Huawei P40 Pro', 'Comme le P40 mais en meilleur', '445', '67', '4');
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('Huawei P40', 'Un bon Huawei mais qui pourrais être mieux !', '320', '743', '4');
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('Huawei Nova 9', 'La gamme Nova est la meilleur du Huawei', '450', '711', '4');
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('Huawei Mate 40 Pro', 'Une révolution dans l\'univers des smartphone !', '720', '399', '4');
+
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('Honor 50', 'Rapide et possède une Supercharge', '450', '49', '5');
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('Honor 50 Lite', 'Comme le 50 mais plus léger et plus lent !', '270', '532', '5');
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('Honor 40', 'La version juste avant le 50 !', '330', '56', '5');
+INSERT INTO `j2e`.`article` (`nom_produit`, `description_produit`, `prix_produit`, `quantite_produit`, `id_categorie`) VALUES ('Honor 40 Lite', 'Un peu anciens mais fait toujours l\'affaire !', '200', '9362', '5');
